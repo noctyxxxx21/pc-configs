6da4376b3926d179e860afa8f6d59f269338429c
noctyxxxx21 <julesbrethou@gmail.com>
2026-08-11 21:34:33 +0200

    Update produits.js

--- produits.js
+++ produits.js
@@ -108,5 +108,185 @@ const produits = [
             }
         ]
     }
+    ,
 
+    {
+        nom: "AMD Ryzen 5 5600",
+        categorie: "Processeurs",
+        specs: "6 cœurs / 12 threads • AM4",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Cdiscount",
+                prix: 124.13,
+                lien: "#"
+            },
+            {
+                nom: "Amazon",
+                prix: 139.28,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 7 5700X",
+        categorie: "Processeurs",
+        specs: "8 cœurs / 16 threads • AM4",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Rakuten",
+                prix: 164.00,
+                lien: "#"
+            },
+            {
+                nom: "Amazon",
+                prix: 199.99,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 7 5700X3D",
+        categorie: "Processeurs",
+        specs: "8 cœurs / 16 threads • AM4",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "LaBonneConfig",
+                prix: 376.00,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 7 5800X3D",
+        categorie: "Processeurs",
+        specs: "8 cœurs / 16 threads • AM4",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Alternate",
+                prix: 354.00,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 5 9600X",
+        categorie: "Processeurs",
+        specs: "6 cœurs / 12 threads • AM5",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Cdiscount",
+                prix: 175.38,
+                lien: "#"
+            },
+            {
+                nom: "Amazon",
+                prix: 183.37,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 7 9700X",
+        categorie: "Processeurs",
+        specs: "8 cœurs / 16 threads • AM5",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Cdiscount",
+                prix: 242.64,
+                lien: "#"
+            },
+            {
+                nom: "Amazon",
+                prix: 265.90,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 9 9900X",
+        categorie: "Processeurs",
+        specs: "12 cœurs / 24 threads • AM5",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Cdiscount",
+                prix: 329.00,
+                lien: "#"
+            },
+            {
+                nom: "Amazon",
+                prix: 338.90,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 9 9950X",
+        categorie: "Processeurs",
+        specs: "16 cœurs / 32 threads • AM5",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Amazon",
+                prix: 533.52,
+                lien: "#"
+            },
+            {
+                nom: "Achatmoinscher",
+                prix: 490.49,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "Intel Core i5-14600KF",
+        categorie: "Processeurs",
+        specs: "14 cœurs / 20 threads • LGA1700",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Cdiscount",
+                prix: 243.84,
+                lien: "#"
+            },
+            {
+                nom: "LaBonneConfig",
+                prix: 249.99,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "Intel Core i7-14700KF",
+        categorie: "Processeurs",
+        specs: "20 cœurs / 28 threads • LGA1700",
+        prixVerifie: "11/08/2026",
+        vendeurs: [
+            {
+                nom: "Amazon",
+                prix: 298.16,
+                lien: "#"
+            },
+            {
+                nom: "Alternate",
+                prix: 381.00,
+                lien: "#"
+            }
+        ]
+    }
 ];
