74c456d851da1b90b2c9e9606b0a114e0a0ee2fb
noctyxxxx21 <julesbrethou@gmail.com>
2026-08-11 20:59:11 +0200

    Create produits.js

---
+++ produits.js
@@ -0,0 +1,55 @@
+const produits = [
+    {
+        nom: "AMD Ryzen 5 7600",
+        categorie: "Processeurs",
+        specs: "6 cœurs / 12 threads • AM5",
+        vendeurs: [
+            {
+                nom: "Amazon",
+                prix: 159.99,
+                lien: "#"
+            },
+            {
+                nom: "Cdiscount",
+                prix: 164.99,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 7 7800X3D",
+        categorie: "Processeurs",
+        specs: "8 cœurs / 16 threads • AM5",
+        vendeurs: [
+            {
+                nom: "Amazon",
+                prix: 349.99,
+                lien: "#"
+            },
+            {
+                nom: "Cdiscount",
+                prix: 359.99,
+                lien: "#"
+            }
+        ]
+    },
+
+    {
+        nom: "AMD Ryzen 7 9800X3D",
+        categorie: "Processeurs",
+        specs: "8 cœurs / 16 threads • AM5",
+        vendeurs: [
+            {
+                nom: "Amazon",
+                prix: 449.99,
+                lien: "#"
+            },
+            {
+                nom: "Cdiscount",
+                prix: 459.99,
+                lien: "#"
+            }
+        ]
+    }
+];
